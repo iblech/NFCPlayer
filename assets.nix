6d801d31160063f5b5ca9cdebbed0410c214313f
Ingo Blechschmidt <iblech@speicherleck.de>
2025-12-18 03:26:21 +0100

    Add example assets description file

---
+++ assets.nix
@@ -0,0 +1,9 @@
+[
+  # Baby Shark
+  {
+    url = "https://www.youtube.com/watch?v=XqZsoesa55w";
+    hash = "sha256-k1OTQ85wmYEElfMHpE6seyjK9Ik5AbwELRIcblYvBpI=";
+    color = "black";
+    tags = [ "aa:bb:cc:dd:ee:ff" ];
+  }
+]
